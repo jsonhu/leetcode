3779dd4e9e41395ecbaaa562e06f3bf962ac2a1b
胡成志 <huchengzhi@huchengzhideMacBook-Pro.local>
2024-10-17 10:25:16 +0800

    feat: first init

---
+++ app/src/main/java/leetcode/dp/MinCostClimbingStairs.java
@@ -0,0 +1,60 @@
+package leetcode.dp;
+
+/**
+ * 给你一个整数数组 cost ，其中 cost[i] 是从楼梯第 i 个台阶向上爬需要支付的费用。一旦你支付此费用，即可选择向上爬一个或者两个台阶。
+ * 你可以选择从下标为 0 或下标为 1 的台阶开始爬楼梯。
+ * 请你计算并返回达到楼梯顶部的最低花费。
+ * 
+ * 示例 1：
+ * 
+ * 输入：cost = [10,15,20]
+ * 输出：15
+ * 解释：你将从下标为 1 的台阶开始。
+ * - 支付 15 ，向上爬两个台阶，到达楼梯顶部。
+ * 总花费为 15 。
+ * 示例 2：
+ * 
+ * 输入：cost = [1,100,1,1,1,100,1,1,100,1]
+ * 输出：6
+ * 解释：你将从下标为 0 的台阶开始。
+ * - 支付 1 ，向上爬两个台阶，到达下标为 2 的台阶。
+ * - 支付 1 ，向上爬两个台阶，到达下标为 4 的台阶。
+ * - 支付 1 ，向上爬两个台阶，到达下标为 6 的台阶。
+ * - 支付 1 ，向上爬一个台阶，到达下标为 7 的台阶。
+ * - 支付 1 ，向上爬两个台阶，到达下标为 9 的台阶。
+ * - 支付 1 ，向上爬一个台阶，到达楼梯顶部。
+ * 总花费为 6 。
+ * 
+ * @param cost
+ * @return
+ */
+public class MinCostClimbingStairs {
+
+    /**
+     * 定义f(i)表示从楼梯的第i个台阶向上爬要支付的最小成本，因为每次可以爬1级或者2级台阶，所以爬上
+     * 第i级台阶既可以从i-1级台阶爬上来也可以从i-2级台阶爬上来，
+     * 
+     * @param cost
+     * @return
+     */
+    public int minCostClimbingStairs(int[] cost) {
+        if (cost == null || cost.length == 0) {
+            return 0;
+        }
+        int n = cost.length;
+        int[] dp = new int[n];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];
+        }
+        return dp[n - 1];
+    }
+
+    public static void main(String[] args) {
+        int[] cost = new int[] { 1, 100, 1, 1, 1, 100, 1, 1, 100, 1 };
+        MinCostClimbingStairs climbingStairs = new MinCostClimbingStairs();
+        int minCost = climbingStairs.minCostClimbingStairs(cost);
+        System.out.println(String.format("minCost: %d", minCost));
+    }
+}
